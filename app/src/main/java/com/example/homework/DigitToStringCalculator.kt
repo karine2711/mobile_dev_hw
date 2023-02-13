84eea209d6c3dc928ab012e3c1e28b854d10989c
Karine Gevorgyan <Karine.Gevorgyan@wolterskluwer.com>
2023-02-13 18:10:30 +0400

    Homework 1

---
+++ app/src/main/java/com/example/homework/DigitToStringCalculator.kt
@@ -0,0 +1,66 @@
+package com.example.homework
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.homework.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class DigitToStringCalculator : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            val digitStr = binding.number.text.toString();
+            if (digitStr.length > 6) {
+                showAlertBox("Number is bigger than 999999!")
+                return@setOnClickListener
+            }
+            val number = digitStr.toInt()
+            if (number < 0) {
+                showAlertBox("Number is negative!")
+            }
+            val answer = IntegerToGrade.constructString(number)
+            binding.answerTextbox.text = answer
+        }
+    }
+
+    private fun showAlertBox(msg: String) {
+        val alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(
+            context
+        )
+        alertDialogBuilder.setTitle("Invalid number");
+        alertDialogBuilder
+            .setMessage(msg)
+            .setCancelable(true)
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/homework/IntegerToGrade.java
@@ -0,0 +1,49 @@
+package com.example.homework;
+
+
+public class IntegerToGrade {
+
+    public static String constructString(int intNumber) {
+        String millions = getStringFor3DigitInt(intNumber / 1_000_000, "million ");
+        String thousands = getStringFor3DigitInt(intNumber % 1_000_000 / 1000, "thousand ");
+        String hundreds = getStringFor3DigitInt(intNumber % 1000);
+        return millions + thousands + hundreds;
+    }
+
+    private static String getStringFor3DigitInt(int threeDigitInt, String suffix) {
+        String strForInt = getStringFor3DigitInt(threeDigitInt);
+        if (strForInt.isEmpty()) {
+            return "";
+        } else {
+            return strForInt + " " + suffix;
+        }
+    }
+
+    private static String getStringFor3DigitInt(int threeDigitInt) {
+        if (threeDigitInt == 0 || threeDigitInt > 999) {
+            return "";
+        }
+        int hundreds = threeDigitInt % 1000 / 100;
+        int tens = threeDigitInt % 100 / 10;
+        int ones = threeDigitInt % 10;
+        String hundredsStr = getStrForDigit(hundreds);
+        String result = hundredsStr.isEmpty() ? "" : hundredsStr + " hundred ";
+        return result + getStringFor2DigitInt(tens, ones);
+    }
+
+    private static String getStringFor2DigitInt(int tens, int ones) {
+        String[] teens = new String[]{"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+        String[] tensStr = new String[]{"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+        if (tens != 1) {
+            return tensStr[tens].isEmpty() ? getStrForDigit(ones) :
+                    tensStr[tens] + " " + getStrForDigit(ones);
+        }
+        return teens[ones];
+    }
+
+    private static String getStrForDigit(int digit) {
+        String[] digits = new String[]{"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        return digits[digit];
+    }
+}
